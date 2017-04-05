8f87445fcb7476d33c859310369344f9815f44e2
Daksh <dakshsrivastava@gmail.com>
2017-04-05 18:13:45 +0530

    Modified google play services config file and added google client api

--- app/src/main/java/com/daksh/wordhunch/Menu/ActivityMainMenu.java
+++ app/src/main/java/com/daksh/wordhunch/Menu/ActivityMainMenu.java
@@ -2,12 +2,17 @@ package com.daksh.wordhunch.Menu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.daksh.wordhunch.R;
 import com.daksh.wordhunch.Rink.Events.RequestChallengeEvent;
 import com.daksh.wordhunch.Rink.RingActivity;
 import com.daksh.wordhunch.Rink.RinkSuggestions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -16,10 +21,13 @@ import org.greenrobot.eventbus.ThreadMode;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class ActivityMainMenu extends AppCompatActivity {
+public class ActivityMainMenu extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     //An instance of RinkSuggestions class to retrieve suggestions
     private RinkSuggestions rinkSuggestions;
+    //The google API client that connects to googl play services for Games
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,18 @@ public class ActivityMainMenu extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        //Build the google api client to connect to Google Play Games
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Games.API)
+                .addScope(Games.SCOPE_GAMES)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        //Initiate the connection
+        mGoogleApiClient.connect();
+
         //Send Event to Request New challenge | Will be received in RinkSuggestions class
         if(EventBus.getDefault().hasSubscriberForEvent(RequestChallengeEvent.class))
             EventBus.getDefault().post(new RequestChallengeEvent(RequestChallengeEvent.RequestMode.CHALLENGE_NETWORK));
@@ -69,5 +89,20 @@ public class ActivityMainMenu extends AppCompatActivity {
     public void onMenuStart() {
         Intent intent = new Intent(this, RingActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
