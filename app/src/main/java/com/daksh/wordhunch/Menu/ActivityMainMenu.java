2c5b0f10eb6c6ce4267ab3fec628f3457a5e19ba
Daksh <dakshsrivastava@gmail.com>
2017-04-06 14:58:17 +0530

    Added GooglePlay Games sign in | WIP : Check leaderboard score to trigger FirstBlood acheievement

--- app/src/main/java/com/daksh/wordhunch/Menu/ActivityMainMenu.java
+++ app/src/main/java/com/daksh/wordhunch/Menu/ActivityMainMenu.java
@@ -1,5 +1,6 @@
 package com.daksh.wordhunch.Menu;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,7 +13,11 @@ import com.daksh.wordhunch.Rink.RingActivity;
 import com.daksh.wordhunch.Rink.RinkSuggestions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.games.Games;
+import com.google.android.gms.games.leaderboard.LeaderboardScore;
+import com.google.android.gms.games.leaderboard.LeaderboardVariant;
+import com.google.android.gms.games.leaderboard.Leaderboards;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -22,11 +27,12 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ActivityMainMenu extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<Leaderboards.LoadPlayerScoreResult> {
 
     //An instance of RinkSuggestions class to retrieve suggestions
     private RinkSuggestions rinkSuggestions;
-    //The google API client that connects to googl play services for Games
+    //The google API client that connects to google play services for Games
     private GoogleApiClient mGoogleApiClient;
 
     @Override
@@ -53,9 +59,14 @@ public class ActivityMainMenu extends AppCompatActivity implements
     protected void onResume() {
         super.onResume();
 
+        //Generate a game options object to set connecting popup as always displayed
+        Games.GamesOptions gamesOptions = Games.GamesOptions.builder()
+                .setShowConnectingPopup(true)
+                .build();
+
         //Build the google api client to connect to Google Play Games
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Games.API)
+                .addApi(Games.API, gamesOptions)
                 .addScope(Games.SCOPE_GAMES)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -91,9 +102,27 @@ public class ActivityMainMenu extends AppCompatActivity implements
         startActivity(intent);
     }
 
+    /**
+     * The menu button takes the user to the about page | Navigates to RinkActivity
+     */
+    @OnClick(R.id.menu_about)
+    public void onMenuAbout() {
+        Intent intent = new Intent(this, ActivityAbout.class);
+        startActivity(intent);
+    }
+
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
+        //Get high score if exists. This would check if the person is playing the game for the first time
+        //or not. Depending on the scenario, a flag is sent to the RinkActivity to inform it to
+        //unlock the FirstBlood(R.string.play_achievement_FirstBlood) achievement.
+        Games.Leaderboards.loadCurrentPlayerLeaderboardScore(
+                mGoogleApiClient,
+                getString(R.string.play_leaderboard_SingleRoundHighScore),
+                LeaderboardVariant.TIME_SPAN_ALL_TIME,
+                LeaderboardVariant.COLLECTION_SOCIAL
+        ).setResultCallback(ActivityMainMenu.this);
     }
 
     @Override
@@ -103,6 +132,24 @@ public class ActivityMainMenu extends AppCompatActivity implements
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityMainMenu.this);
 
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder
+                .setMessage(connectionResult.getErrorMessage())
+                .setTitle(connectionResult.getErrorCode());
+
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onResult(@NonNull Leaderboards.LoadPlayerScoreResult loadPlayerScoreResult) {
+//        if(loadPlayerScoreResult.getStatus() == )
+        //Extract the highest score for the scoreboard for which this request was received
+        //Single Round HighScore (R.string.play_leaderboard_SingleaRoundHighScore)
+        LeaderboardScore scoreboard = loadPlayerScoreResult.getScore();
     }
 }
